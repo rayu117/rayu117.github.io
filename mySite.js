9d1d7cee45ec938c4d6303b47608e0a98f961094
Raymond Udeogu <rfizzle25@gmail.com>
2021-03-18 16:02:59 -0400

    update

---
+++ mySite.js
@@ -0,0 +1,9 @@
+//create nav bar that ALWAYS stays in display 
+    //when a category is clicked the user should 
+        //use scroll into view OR scroll top
+            //possibly mouse coordinate tracker
+
+
+document.addEventListener("DOMContentLoaded", () =>{
+
+})
