f39260296364c14173d8be399812d47a50ac3522
Raymond Udeogu <rfizzle25@gmail.com>
2021-03-21 18:27:00 -0400

    added dark mode

--- mySite.js
+++ mySite.js
@@ -1,9 +1,5 @@
-//create nav bar that ALWAYS stays in display 
-    //when a category is clicked the user should 
-        //use scroll into view OR scroll top
-            //possibly mouse coordinate tracker
-
-
-document.addEventListener("DOMContentLoaded", () =>{
-
-})
+function darkFunction() {
+    let element = document.body;
+    element.classList.toggle("dark-mode")
+}
+darkFunction()
