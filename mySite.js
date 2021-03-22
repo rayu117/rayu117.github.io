62cb28d24a6acb1840e5c08004cde9d25cff4e89
Raymond Udeogu <rfizzle25@gmail.com>
2021-03-22 00:46:54 -0400

    updated projects and added some JS

--- mySite.js
+++ mySite.js
@@ -2,4 +2,29 @@ function darkFunction() {
     let element = document.body;
     element.classList.toggle("dark-mode")
 }
-darkFunction()
+
+function snowmanShow() {
+    let newInfo = document.createElement("p");
+    newInfo.textContent = "The Snowman Game is a guessing game run using node.js. The object of this project was to show mastery in the use of objects.";
+    const mySnowShow = document.getElementById("new-Snow-Info")
+    mySnowShow.appendChild(newInfo)
+}
+
+// document.addEventListener("DOMContentLoaded", () =>{
+//     let test = document.getElementById("test");
+//     test.addEventListener("mouseover", () =>{
+//         // let adventure = document.createElement("")
+//         let newInfo = document.createElement("p");
+//     newInfo.textContent = "The Snowman Game objective of this project was to manipulate objects.";
+//     const myShow = document.getElementById("new-Info")
+//     myShow.appendChild(newInfo)
+
+//     })
+// })
+
+function textAdventureShow() {
+    let textInfo = document.createElement("p");
+    textInfo.textContent = "The Luxuey Car Builder is a text-based game run using node.js. The object of this project was to show mastery in the use of functions and control flow.";
+    const myTextShow = document.getElementById("new-Text-Info")
+    myTextShow.appendChild(textInfo)
+}
