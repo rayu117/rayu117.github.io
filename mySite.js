d0257dddcf174fc0e8f70fe7bb48d441d80516e6
Raymond Udeogu <rfizzle25@gmail.com>
2021-08-11 15:37:32 -0400

    add max-heght to cover image

--- mySite.js
+++ mySite.js
@@ -1,13 +1,14 @@
 function darkFunction() {
-    let element = document.body;
-    element.classList.toggle("dark-mode")
+  let element = document.body;
+  element.classList.toggle("dark-mode");
 }
 
 function snowmanShow() {
-    let newInfo = document.createElement("p");
-    newInfo.textContent = "The Snowman Game is a guessing game run using node.js. The object of this project was to show mastery in the use of objects.";
-    const mySnowShow = document.getElementById("new-Snow-Info")
-    mySnowShow.appendChild(newInfo)
+  let newInfo = document.createElement("p");
+  newInfo.textContent =
+    "The Snowman Game is a guessing game run using node.js. The object of this project was to show mastery in the use of objects.";
+  const mySnowShow = document.getElementById("new-Snow-Info");
+  mySnowShow.appendChild(newInfo);
 }
 
 // document.addEventListener("DOMContentLoaded", () =>{
@@ -23,8 +24,21 @@ function snowmanShow() {
 // })
 
 function textAdventureShow() {
-    let textInfo = document.createElement("p");
-    textInfo.textContent = "The Luxuey Car Builder is a text-based game run using node.js. The object of this project was to show mastery in the use of functions and control flow.";
-    const myTextShow = document.getElementById("new-Text-Info")
-    myTextShow.appendChild(textInfo)
+  let textInfo = document.createElement("p");
+  textInfo.textContent =
+    "The Luxuey Car Builder is a text-based game run using node.js. The object of this project was to show mastery in the use of functions and control flow.";
+  const myTextShow = document.getElementById("new-Text-Info");
+  myTextShow.appendChild(textInfo);
 }
+
+// function showProject(project) {
+//     let project = document.createElement("p");
+//     if (project === textAdv) {
+//         project.textContent = "The Luxuey Car Builder is a text-based game run using node.js. The object of this project was to show mastery in the use of functions and control flow.";
+//     } else {
+//         project.textContent = "The Snowman Game is a guessing game run using node.js. The object of this project was to show mastery in the use of objects.";
+//     }
+//     const myTextShow = document.getElementById("new-project-info")
+//     myTextShow.appendChild(project)
+
+// }
